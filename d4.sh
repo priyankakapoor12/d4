dfc7aae83208d4c60b67967f0f7cc5a2c6db89dc
priyankakapoor <priyankakapoor1993@gmail.com>
2025-11-04 15:02:37 +0530

    by me

---
+++ d4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Please enter you Username: "
+read USERNAME
+echo "username entered = $USERNAME"
+
+echo "Please enter Password: "
+read -s PASSWORD
+echo
+echo "Password entered = $PASSWORD"
